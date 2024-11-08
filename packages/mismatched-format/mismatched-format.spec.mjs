7858a50321b244a2f2c88155438972ee64321e96
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-11-08 12:35:30 +0000

    test: add e2e cases for `mismatched-format`

--- packages/mismatched-format/mismatched-format.spec.mjs
+++ packages/mismatched-format/mismatched-format.spec.mjs
@@ -8,54 +8,94 @@ import {
 
 
 describe('Mismatched format loader', () => {
-	let load;
-	let mock__containsCJS;
+	describe('unit', () => {
+		let load;
+		let mock__containsCJS;
 
-	before(async () => {
-		const containsCJS = mock.fn();
-		mock__containsCJS = containsCJS.mock;
-		mock.module('./contains-cjs.mjs', {
-			namedExports: { containsCJS },
+		before(async () => {
+			const containsCJS = mock.fn();
+			mock__containsCJS = containsCJS.mock;
+			mock.module('./contains-cjs.mjs', {
+				namedExports: { containsCJS },
+			});
+
+			({ load } = await import('./mismatched-format.mjs'));
 		});
 
-		({ load } = await import('./mismatched-format.mjs'));
+		describe('when "esm" is actually cjs', () => {
+			it('should detect and report the corrected format', async () => {
+				mock__containsCJS.mockImplementationOnce(() => true)
+				const result = await load(
+					import.meta.resolve('./unimportant.js'),
+					{},
+					async () => ({
+						format: 'module',
+						source: '"unimportant"',
+					}),
+				);
+
+				assert.equal(result.format, 'commonjs');
+			});
+
+			it('should detect and report the corrected format', async () => {
+				mock__containsCJS.mockImplementationOnce(() => false)
+				const result = await load(
+					import.meta.resolve('./unimportant.js'),
+					{},
+					async () => { throw new Error('require and import') },
+				);
+
+				assert.equal(result.format, 'commonjs');
+			});
+
+			it('should detect and report the corrected format', async () => {
+				mock__containsCJS.mockImplementationOnce(() => false)
+				const result = await load(
+					import.meta.resolve('./unimportant.js'),
+					{},
+					async () => { throw new Error('CommonJS') },
+				);
+
+				assert.equal(result.format, 'commonjs');
+			});
+		});
 	});
 
-	describe('when "esm" is actually cjs', () => {
-		it('should detect and report the corrected format', async () => {
-			mock__containsCJS.mockImplementationOnce(() => true)
-			const result = await load(
-				import.meta.resolve('./unimportant.js'),
-				{},
-				async () => ({
-					format: 'module',
-					source: '"unimportant"',
-				}),
-			);
+	describe('e2e', () => {
+		let load;
+		let nextLoad;
 
-			assert.equal(result.format, 'commonjs');
+		before(async () => {
+			({ load } = await import('./mismatched-format.mjs'));
+			({ nextLoad } = await import('../../fixtures/nextLoad.fixture.mjs'));
 		});
 
-		it('should detect and report the corrected format', async () => {
-			mock__containsCJS.mockImplementationOnce(() => false)
-			const result = await load(
-				import.meta.resolve('./unimportant.js'),
-				{},
-				async () => { throw new Error('require and import') },
-			);
+		describe('correctly identify the containing CJS as CJS', () => {
+			it('should handle createRequire', async () => {
+				const result = await load(import.meta.resolve('./fixtures/actually-cjs/uses-require.cjs.js'), {}, nextLoad);
 
-			assert.equal(result.format, 'commonjs');
+				assert.equal(result.format, 'commonjs');
+			});
+
+			it('should handle `require()` within a comment', async () => {
+				const result = await load(import.meta.resolve('./fixtures/actually-cjs/module-exports.cjs.js'), {}, nextLoad);
+
+				assert.equal(result.format, 'commonjs');
+			});
 		});
 
-		it('should detect and report the corrected format', async () => {
-			mock__containsCJS.mockImplementationOnce(() => false)
-			const result = await load(
-				import.meta.resolve('./unimportant.js'),
-				{},
-				async () => { throw new Error('CommonJS') },
-			);
+		describe('correctly identify the containing ESM as ESM', () => {
+			it('should handle createRequire', async () => {
+				const result = await load(import.meta.resolve('./fixtures/actually-esm/create-require.esm.js'), {}, nextLoad);
 
-			assert.equal(result.format, 'commonjs');
+				assert.equal(result.format, 'module');
+			});
+
+			it('should handle `require()` within a comment', async () => {
+				const result = await load(import.meta.resolve('./fixtures/actually-esm/require-in-comment.esm.js'), {}, nextLoad);
+
+				assert.equal(result.format, 'module');
+			});
 		});
 	});
 });
