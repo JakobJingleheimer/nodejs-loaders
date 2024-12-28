09e73dcff481c76629a2e58c1c52c1745e3b968a
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-12-28 12:16:24 +0100

    test: convert e2e test from `spawnPromisified` → `spawnSync`

--- packages/alias/alias.test.mjs
+++ packages/alias/alias.test.mjs
@@ -1,19 +1,19 @@
 import assert from 'node:assert/strict';
+import { spawnSync } from 'node:child_process';
 import { execPath } from 'node:process';
 import { describe, it } from 'node:test';
 import { fileURLToPath } from 'node:url';
-
-import { spawnPromisified } from '../../utils/spawn-promisified.mjs';
 
 import { message } from './fixtures/message.mjs';
 
 describe('alias (e2e)', () => {
 	const cwd = fileURLToPath(import.meta.resolve('./fixtures'));
+	const encoding = 'utf-8';
 	const e2eTest = fileURLToPath(import.meta.resolve('./fixtures/e2e.mjs'));
 	const msgRgx = new RegExp(message);
 
-	it('should work with `--loader`', async () => {
-		const { code, stderr, stdout } = await spawnPromisified(
+	it('should work with `--loader`', () => {
+		const { status: code, stderr, stdout } = spawnSync(
 			execPath,
 			[
 				'--no-warnings',
@@ -21,7 +21,10 @@ describe('alias (e2e)', () => {
 				fileURLToPath(import.meta.resolve('./alias.mjs')),
 				e2eTest,
 			],
-			{ cwd },
+			{
+				cwd,
+				encoding,
+			},
 		);
 
 		assert.equal(stderr, '');
@@ -29,8 +32,8 @@ describe('alias (e2e)', () => {
 		assert.equal(code, 0);
 	});
 
-	it('should work with `module.register`', async () => {
-		const { code, stderr, stdout } = await spawnPromisified(
+	it('should work with `module.register`', () => {
+		const { status: code, stderr, stdout } = spawnSync(
 			execPath,
 			[
 				'--no-warnings',
@@ -38,7 +41,10 @@ describe('alias (e2e)', () => {
 				fileURLToPath(import.meta.resolve('./fixtures/register.mjs')),
 				e2eTest,
 			],
-			{ cwd },
+			{
+				cwd,
+				encoding,
+			},
 		);
 
 		assert.equal(stderr, '');
@@ -47,8 +53,8 @@ describe('alias (e2e)', () => {
 	});
 
 	if (process.version.startsWith('v23')) {
-		it('should work with `module.registerHooks`', async () => {
-			const { code, stderr, stdout } = await spawnPromisified(
+		it('should work with `module.registerHooks`', () => {
+			const { status: code, stderr, stdout } = spawnSync(
 				execPath,
 				[
 					'--no-warnings',
@@ -56,7 +62,10 @@ describe('alias (e2e)', () => {
 					fileURLToPath(import.meta.resolve('./fixtures/register-hooks.mjs')),
 					e2eTest,
 				],
-				{ cwd },
+				{
+					cwd,
+					encoding,
+				},
 			);
 
 			assert.equal(stderr, '');
